b606ee09b118fba7e45a2257c4f8224fa0fa48d4
Dmitry Nagovitsin <count0ru@gmail.com>
2018-01-12 13:42:43 +0300

    Update stack_up.sh

--- stack_up.sh
+++ stack_up.sh
@@ -5,7 +5,6 @@ else
   echo "usage: stack_up.sh <namespace>"
   exit
 fi
-kubectl create -f liferegistry_secret.yaml $NAMESPACE 
 kubectl create -f zookeeper.yaml $NAMESPACE 
 while [ $(kubectl get po $NAMESPACE -o=wide | grep zk | grep Run | wc -l) -ne 3 ] ; do sleep 3; echo "Zookeper cluster not ready. Will try again after 3 sec..." ;done
 kubectl create -f clickhouse.yaml $NAMESPACE 
